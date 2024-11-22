3b256d8ec9030da0b58ea47f8916f18ab930b51d
Munzir-Widaa <munzir.widaa@stu.ocsb.ca>
2024-11-22 10:30:18 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,20 @@
 scene.setBackgroundColor(13)
 let mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . b 5 b . . . 
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . 3 3 3 3 3 3 b . . . . 
-    . . . . . 3 3 5 5 5 5 5 b . . . 
-    . 3 3 3 3 3 5 5 5 5 5 5 5 b . . 
-    . 3 d 3 b 5 5 5 5 5 5 5 5 b . . 
-    . 3 3 5 5 b 5 d 1 f 5 d 4 f . . 
-    . 3 3 d 5 5 b 1 f f 5 4 4 c . . 
-    3 3 3 b 5 5 5 d f b 4 4 4 4 4 b 
-    3 3 3 c d 5 5 b 5 4 4 4 4 4 3 . 
-    3 3 3 3 3 3 b 5 5 5 5 5 5 5 3 . 
-    3 3 3 3 3 3 d 5 5 5 5 5 5 5 3 . 
-    3 3 3 3 3 3 3 d 5 5 5 5 5 d 3 . 
-    . 3 3 3 3 3 3 3 3 5 5 5 3 3 3 . 
-    . . 3 3 3 3 3 3 3 3 3 3 3 3 . . 
+    . . . . f f f f f f . . . . . . 
+    . . . f 2 f e e e e f f . . . . 
+    . . f 2 2 2 f e e e e f f . . . 
+    . . f e e e e f f e e e f . . . 
+    . f e 2 2 2 2 e e f f f f . . . 
+    . f 2 e f f f f 2 2 2 e f . . . 
+    . f f f e e e f f f f f f f . . 
+    . f e e 4 4 f b e 4 4 e f f . . 
+    . . f e d d f 1 4 d 4 e e f . . 
+    . . . f d d d d 4 e e e f . . . 
+    . . . f e 4 4 4 e d d 4 . . . . 
+    . . . f 2 2 2 2 e d d e . . . . 
+    . . f f 5 5 4 4 f e e f . . . . 
+    . . f f f f f f f f f f . . . . 
+    . . . f f f . . . f f . . . . . 
     `, SpriteKind.Player)
 controller.moveSprite(mySprite, 90, 90)
